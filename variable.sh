9a99cc1904d222711d93fd388a9c87970aed7c1a
Hakeem <Hakeem@gmail.com>
2024-05-17 21:21:47 +0530

    shell script4

--- variable.sh
+++ variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PERSON1=HAKEEM
-PERSON2=RAMU
+PERSON2=Hasna
 
-echo "$PERSON1::: halo hai $PERSON2"
-echo "$PERSON2 ::: ha hai $PERSON1"
+echo "$PERSON1:: halo hai $PERSON2"
+echo "$PERSON2 :: ha hai $PERSON1"
 echo "$PERSON1:hi how are you $PERSON2"
-echo "$PERSON2 I am fine how about you $PERSON1"
+echo "$PERSON2:I am fine how about you $PERSON1"
