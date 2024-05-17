1e8e134b0b053cd5b510f8e1f69dfa224c0b1178
Hakeem <Hakeem@gmail.com>
2024-05-17 21:36:26 +0530

    shell script6

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: halo hai $PERSON2"
+echo "$PERSON2 :: ha hai $PERSON1"
+echo "$PERSON1:hi how are you $PERSON2"
+echo "$PERSON2:I am fine how about you $PERSON1"
