79622cb822450071ddd31cbe5d41b6daf21f451b
Hakeem <Hakeem@gmail.com>
2024-05-18 14:38:06 +0530

    shellscript23

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,8 +4,17 @@
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    #exit 2
+    exit 2
 else
      echo "your root user"
 fi
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+ echo "ERROR :: Installed Mysql is faild"
+ exit 1
+ else 
+ echo " Installed Mysql is success"
+ 
+ fi
