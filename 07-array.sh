53a61e96d77a69049d52add74a4a0b71b99fbc13
Hakeem <Hakeem@gmail.com>
2024-05-18 11:40:58 +0530

    shellscript3

--- 07-array.sh
+++ 07-array.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
  
-FRUITS=("apple" "mango","banana")
+FRUITS=("apple" "mango" "banana")
 
 echo "first fruit:: ${FRUITS[0]}"
 echo "second fruit:: ${FRUITS[1]}"
 echo "thard fruit:: ${FRUITS[2]}"
+
+echo "All fruits::${FRUITS[@]}"
