621a4ef4c3317a6b0951bff2c326f7ad6ff872c6
Hakeem <Hakeem@gmail.com>
2024-05-18 17:00:03 +0530

    shellscript25

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,14 +4,20 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+ R= "\e[31m"
+ G= "\e[32m"
+ Y="\e[33m"
+ N="\e[0m"
+
+
     VALIDATE (){
 
     if [ $1 -ne 0 ]
 then
-    echo "ERROR :: $2 ..is faild"
+    echo -e "ERROR :: $2 ..is $R faild $N"
     exit 1
  else 
-    echo " $2 ..is success"
+    echo -e " $2 ..is $G success $N"
 
  fi
 }
